b703d0dec75b9addca9da782026b5a59755f7058
Evllooew <evllooev@icloud.com>
2024-02-21 20:52:35 +0300

    bot

---
+++ src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -0,0 +1,22 @@
+package pro.sky.telegrambot.configuration;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.DeleteMyCommands;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TelegramBotConfiguration {
+
+    @Value(value = "6267536754:AAE-saN6vKjjTAN-a3ziwirqsl_PZP-AFOA")
+    private String token;
+
+    @Bean
+    public TelegramBot telegramBot() {
+        TelegramBot bot = new TelegramBot(token);
+        bot.execute(new DeleteMyCommands());
+        return bot;
+    }
+
+}
